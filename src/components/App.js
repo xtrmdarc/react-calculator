9992671e11b3e051f7b1cc136407854025aeba2a
xtrmdarc <diegoreyesco@hotmail.com>
2020-07-17 12:11:37 -0500

    Create componentes folder and related files and setup app component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+const App = () => {
+  return (
+    <div>
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+}
+ 
+export default App;
