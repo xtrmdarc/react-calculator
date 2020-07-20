c4386022c1e25939686ad6b95dbbd140d33d852b
xtrmdarc <diegoreyesco@hotmail.com>
2020-07-20 17:33:55 -0500

    Implement calculate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,8 +1,42 @@
 import operate from './operate';
 
-const calculate = (calc, buttonName) => {
-  if (['+', '-', 'X', '%', '÷'].includes(buttonName))
-    return operate(calc.total, calc.next, buttonName);
+const calculate = ({ total, next, operation }, buttonName) => {
+
+  if (['+', '-', 'X', '%', '÷'].includes(buttonName)) {
+    total = operate(total, next, operation);
+    operation = buttonName;
+    next = null;
+  }
+
+  if (parseInt(buttonName) || buttonName == '.') {
+    if (operation == null) {
+      total = `${total ? total : ''}${buttonName}`;
+    }
+    else {
+      next = `${next ? next : ''}${buttonName}`;
+    }
+  }
+
+  switch (buttonName) {
+    case '+/-' : {
+      total *= -1;
+      next *= -1;
+      break;
+    }
+    case 'AC' : {
+      total = 0;
+      next = null;
+      operation = null;
+      break;
+    }
+    case '=' : {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+    default: break;
+  }
+  return { total, next, operation };
 };
 
 export default calculate;
